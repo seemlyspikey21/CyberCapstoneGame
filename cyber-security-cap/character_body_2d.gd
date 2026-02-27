fa55df0bdbc7afcc5648e27da1dcd414dfb082e8
Vincent <linv268@gmail.com>
2026-02-27 13:15:55 -0500

    Add files via upload

---
+++ cyber-security-cap/character_body_2d.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export var speed: float = 200.0
+
+func _physics_process(delta):
+	var direction := Vector2.ZERO
+
+	if Input.is_action_pressed("right"):
+		direction.x += 1
+	if Input.is_action_pressed("left"):
+		direction.x -= 1
+	if Input.is_action_pressed("down"):
+		direction.y += 1
+	if Input.is_action_pressed("up"):
+		direction.y -= 1
+
+	velocity = direction.normalized() * speed
+	move_and_slide()
